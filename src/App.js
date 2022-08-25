c9d62fe47301157f9984d4a1d0f34de75fbf245a
Sander <sandert.kodehode@gmail.com>
2022-08-25 20:29:22 +0200

    CMS added and working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+// import { CompanyList } from './components/companyList'
+import FetchData from './components/fetchData'
+
 function App() {
 	return (
-    <div className='App'>
-      <h1>fsdf</h1>
+		<div className='App'>
+			<h1>Bedriftregister</h1>
+			<FetchData />
 		</div>
 	)
 }
