01154434b7d788259e2ff8cb34872b17343b1db9
Sander Tonheim <sandert.kodehode@gmail.com>
2022-08-29 12:53:06 +0200

    init

--- src/components/FetchData/fetchData.js
+++ src/components/FetchData/fetchData.js
@@ -1,44 +1,6 @@
 import { StyledFetchDataContainer } from './Style/FetchData.styled'
 import { StyledLi } from './../FetchData/Style/FetchData.li.styled'
 
-// function FetchData() {
-// 	let PROJECT_ID = 'oans3nsr'
-// 	let DATASET = 'production'
-// 	let QUERY = encodeURIComponent('*[_type == "bedrift"]')
-
-// 	// Compose the URL for your project's endpoint and add the query
-// 	let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`
-// 	// Select the DOM-element, so that you can replace it with content
-// 	// fetch the content
-// 	fetch(URL)
-// 		.then((res) => res.json())
-// 		.then(({ result }) => {
-// 			// get the list element, and the first item
-// 			let list = document.querySelector('ul')
-// 			let firstListItem = document.querySelector('ul StyledLi')
-
-// 			if (result.length > 0) {
-// 				// remove the placeholder content
-// 				list.removeChild(firstListItem)
-// 				result.forEach((bedrift) => {
-// 					// create a list element for each animal
-// 					let listItem = document.createElement('StyledLi')
-
-// 					// add the company name as the text content
-// 					listItem.textContent = bedrift?.name
-// 					// add the item to the list
-// 					list.appendChild(listItem)
-// 					return (
-// 						// <StyledFetchDataContainer>
-// 						<ul>
-// 							<StyledLi>Loading animals…</StyledLi>
-// 						</ul>
-// 						/* </StyledFetchDataContainer> */
-// 					)
-// 				})
-// 			}
-// 		})
-// }
 
 function FetchData() {
 	// Select the DOM-element, so that you can replace it with content
