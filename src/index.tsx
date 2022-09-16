3572d0b74d8e0a76a6a74d6e75a39678c970b0ad
Sander Tonheim <sandert.kodehode@gmail.com>
2022-09-16 13:47:05 +0200

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ReactDOM } from 'react'
 
-const root = ReactDOM.createRoot(document.getElementById('root')!)
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
 
 // If you want to start measuring performance in your app, pass a function
